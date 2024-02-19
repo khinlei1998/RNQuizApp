e5870879079a0594bdb168b448eef22b03c4e5f7
khinlei1998 <khinleiyatihtun98@gmail.com>
2024-02-19 15:53:58 +0630

    create context

--- App.tsx
+++ App.tsx
@@ -1,15 +1,37 @@
 
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import { SafeAreaView, Text, StatusBar } from 'react-native';
 import StartScreen from './src/screens/StartScreen';
 import GameScreen from './src/screens/GameScreen';
+// import { UseStartContext } from './src/Context/Context';
+interface StartScreenProps {
+  startGame: () => void;
+}
+
+// Step 2: Create a context for the start screen
+const StartContext = createContext<StartScreenProps | undefined>(undefined);
+
+// Step 3: Create a component that consumes the context
+const ThemeContext = () => useContext(StartContext);
+
 function App(): React.JSX.Element {
+  const startGame = () => {
+    console.log('Starting the game...');
+    // Add logic to start the game
+  };
   const [isPlaying, setPlaying] = useState<boolean>(false)
+  // const [theme, setTheme] = useState<Theme>('light');
   return (
     <>
       <StatusBar barStyle="light-content" />
       <SafeAreaView>
-        {!isPlaying ? <StartScreen /> : <GameScreen />}
+        <StartContext.Provider value={{ startGame }}>
+          <StartScreen />
+        </StartContext.Provider>
+        {/* <UseStartContext.Provider value={{ startGame }}>
+          <StartScreen />
+        </UseStartContext.Provider> */}
+
 
       </SafeAreaView>
     </>
